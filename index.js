c96e898d26f537c568b6e4dedae79f5aa15c6b8e
siroi <1152910080@qq.com>
2024-09-09 14:50:42 +0800

    ✨ feat(hooks): 支持多个 request 请求

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function useRaceRequest(request) {
             //* 说明该方法只有 options 参数
             return request(Object.assign(Object.assign({}, args[0]), { signal: controller.signal }));
         }
-        if (args.length === (request.length - 1)) {
+        if (args.length === request.length - 1) {
             //* 说明用户没传 options
             return request(...args, options);
         }
@@ -27,3 +27,33 @@ function useRaceRequest(request) {
     return newRequest;
 }
 export default useRaceRequest;
+function useRaceRequests(requests) {
+    const umiCancels = useRef({});
+    const newRequests = requests.map((request) => {
+        return (...args) => {
+            if (umiCancels.current && umiCancels.current[request.name]) {
+                umiCancels.current[request.name].abort();
+            }
+            const controller = new AbortController();
+            umiCancels.current = Object.assign(Object.assign({}, umiCancels.current), { [request.name]: controller });
+            let options = { signal: controller.signal };
+            if (request.length === 1) {
+                //* 说明该方法只有 options 参数
+                return request(Object.assign(Object.assign({}, args[0]), { signal: controller.signal }));
+            }
+            if (args.length === request.length - 1) {
+                //* 说明用户没传 options
+                return request(...args, options);
+            }
+            else {
+                //* 说明有 options
+                //* 判断 options 是一个对象，获取 options 参数并进行拼接
+                options = args[args.length - 1];
+                options = Object.assign(Object.assign({}, options), { signal: controller.signal });
+                return request(...args.slice(0, args.length - 1), options);
+            }
+        };
+    });
+    return newRequests;
+}
+export { useRaceRequests };
--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,73 @@
 import { useRef } from 'react';
 
-function useRaceRequest<T extends (...args: any[]) => any>(request: T): T{
+type Request = (...args: any[]) => any;
 
-    const umiCancel = useRef<AbortController | null>(null);
+function useRaceRequest<T extends Request>(request: T): T {
+  const umiCancel = useRef<AbortController | null>(null);
 
-    const newRequest = (...args: Parameters<T>) => {
-        if(umiCancel.current){
-            umiCancel.current.abort();
-        }
-        const controller = new AbortController();
-        umiCancel.current = controller;
-        let options = {signal: controller.signal};
+  const newRequest = (...args: Parameters<T>) => {
+    if (umiCancel.current) {
+      umiCancel.current.abort();
+    }
+    const controller = new AbortController();
+    umiCancel.current = controller;
+    let options = { signal: controller.signal };
 
-        if(request.length === 1){
-           //* 说明该方法只有 options 参数
-           return request({...args[0], signal: controller.signal})
-        }
-
-        if(args.length === (request.length - 1)){
-            //* 说明用户没传 options
-            return request(...args, options);
-        }else{
-            //* 说明有 options
-            //* 判断 options 是一个对象，获取 options 参数并进行拼接
-            options = args[args.length-1];
-            options = {...options, signal: controller.signal};
-            return request(...args.slice(0, args.length-1), options);
-        }
+    if (request.length === 1) {
+      //* 说明该方法只有 options 参数
+      return request({ ...args[0], signal: controller.signal });
     }
 
-    return newRequest as T;
+    if (args.length === request.length - 1) {
+      //* 说明用户没传 options
+      return request(...args, options);
+    } else {
+      //* 说明有 options
+      //* 判断 options 是一个对象，获取 options 参数并进行拼接
+      options = args[args.length - 1];
+      options = { ...options, signal: controller.signal };
+      return request(...args.slice(0, args.length - 1), options);
+    }
+  };
 
+  return newRequest as T;
 }
 
 export default useRaceRequest;
+
+function useRaceRequests<T extends Request[]>(requests: T): T {
+  const umiCancels = useRef<Record<string, AbortController>>({});
+
+  const newRequests = requests.map((request) => {
+    return (...args: Parameters<T[number]>) => {
+      if (umiCancels.current && umiCancels.current[request.name]) {
+        umiCancels.current[request.name].abort();
+      }
+      const controller = new AbortController();
+      umiCancels.current = {
+        ...umiCancels.current,
+        [request.name]: controller,
+      };
+      let options = { signal: controller.signal };
+      if (request.length === 1) {
+        //* 说明该方法只有 options 参数
+        return request({ ...args[0], signal: controller.signal });
+      }
+
+      if (args.length === request.length - 1) {
+        //* 说明用户没传 options
+        return request(...args, options);
+      } else {
+        //* 说明有 options
+        //* 判断 options 是一个对象，获取 options 参数并进行拼接
+        options = args[args.length - 1];
+        options = { ...options, signal: controller.signal };
+        return request(...args.slice(0, args.length - 1), options);
+      }
+    };
+  });
+
+  return newRequests as T;
+}
+
+export { useRaceRequests };
